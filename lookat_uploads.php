5b232dca92010fdce8502be476e0bc12c3c8710e
Eric Ford <ericford@mac.com>
2021-04-16 14:30:52 -0400

    Still trying to see if it's writing correctly into uploads.

--- lookat_uploads.php
+++ lookat_uploads.php
@@ -1,5 +1,5 @@
 <?php
-$directory = "/uploads";
+$directory = "/uploads/results";
 
 // Open a directory, and read its contents
 if (is_dir($directory)){
@@ -11,21 +11,21 @@ if (is_dir($directory)){
   }
 }
 
-$myfile = fopen("$directory/testfile.txt", "w");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);
+// $myfile = fopen("$directory/testfile.txt", "w");
+// $txt = "John Doe\n";
+// fwrite($myfile, $txt);
+// $txt = "Jane Doe\n";
+// fwrite($myfile, $txt);
+// fclose($myfile);
 
 
-if (is_dir($directory)){
-  if ($opendirectory = opendir($directory)){
-    while (($file = readdir($opendirectory)) !== false){
-      echo "filename:" . $file . "<br>";
-    }
-    closedir($opendirectory);
-  }
-}
+// if (is_dir($directory)){
+//   if ($opendirectory = opendir($directory)){
+//     while (($file = readdir($opendirectory)) !== false){
+//       echo "filename:" . $file . "<br>";
+//     }
+//     closedir($opendirectory);
+//   }
+// }
 
 ?>
