7c76097bc9a1d4096dfad00db52b3e36aa81ef64
Eric Ford <ericford@mac.com>
2021-04-16 14:03:08 -0400

    Test reading/writing to /uploads.

--- lookat_uploads.php
+++ lookat_uploads.php
@@ -1,5 +1,5 @@
 <?php
-$directory = "/uploads";
+$directory = "/uploads/results";
 
 // Open a directory, and read its contents
 if (is_dir($directory)){
@@ -10,4 +10,25 @@ if (is_dir($directory)){
     closedir($opendirectory);
   }
 }
+
+$directory = "/uploads/results";
+
+// Open a directory, and read its contents
+if (is_dir($directory)){
+    $myfile = fopen("$directory/testfile.txt", "w");
+    $txt = "John Doe\n";
+    fwrite($myfile, $txt);
+    $txt = "Jane Doe\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
+}
+if (is_dir($directory)){
+  if ($opendirectory = opendir($directory)){
+    while (($file = readdir($opendirectory)) !== false){
+      echo "filename:" . $file . "<br>";
+    }
+    closedir($opendirectory);
+  }
+}
+
 ?>
