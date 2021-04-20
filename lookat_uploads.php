29ff52fb55d410e3fdd6da265d39f4a2b870e392
Eric Ford <ericford@mac.com>
2021-04-20 10:45:50 -0400

    Making lookat_uploads.php have a prettier interface and require a password.

--- lookat_uploads.php
+++ lookat_uploads.php
@@ -1,59 +1,82 @@
+<!doctype html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Project Inclusive</title>
+    <meta name="description" content="MinnoJS">
+    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="user-scalable=no, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1" />
+    <meta name="pinterest" content="nopin" />
+
+    <link rel="stylesheet" href="minno-css/main.css" />
+    <link rel="stylesheet" href="minno-css/mine.css" />
+  </head>
+  <body>
+    <div class="container ng-scope simple-skin" id="pi-app">
+      <img class="pi-spinner ng-hide" ng-hide="1">
+      <div pi-swap="" ng-class="{'pi-spinner':loading}" pi-manager="study/mgr.js?random=607cef919c0da" class="">
+        <div pi-task="task" class="ng-scope ng-isolate-scope">
+          <div pi-timer="" pi-quest="" class="ng-scope">
+            <h3 ng-if="page.header" ng-bind-html="page.header" ng-style="page.headerStyle" class="ng-binding ng-scope">
+              <div class="panel panel-info">
+                <div class="panel-body">
+                  <img src="./study/media/P-I_logo.png" class="p-i-logo" />
+                  <h1>Clinical Decision-Making in the Prenatal Setting v8</h1>
 <?php
-$version = 'v7';
-$directory = '/uploads/results';
-echo "$version";
+  ini_set('display_startup_errors', 1);
+  ini_set('display_errors', 1);
+  error_reporting(-1);
 
-echo '<h1>Initial contents:</h1>';
-if (is_dir($directory)){
-  if ($opendirectory = opendir($directory)) {
-    while (($file = readdir($opendirectory)) !== false) {
-      echo "filename: $file<br />";
-    }
-    closedir($opendirectory);
-  } else {
-      echo "<strong><em>$directory is missing!</em></strong>";
-  }
-}
-
-echo "<h1>Now show individual directories’ contents.</h1>";
-$arr = array('initial_participants', 'feedback', 'answers');
-foreach ($arr as $this_dir) {
-    echo "<h3>$this_dir:</h3>";
-    $cur_dir = "$directory/$this_dir";
-    if (is_dir($cur_dir)){
-      if ($opendirectory = opendir($cur_dir)) {
-        while (($file = readdir($opendirectory)) !== false) {
-          echo "filename: $file<br />";
-        }
-        closedir($opendirectory);
-      } else {
-          echo "<em><strong>$cur_dir is missing!</strong></em>";
-      }
-    }
-}
-
-echo '<h1>Now delete initial_participants:</h1>';
-// Have to delete individual files, then delete directory.
-if ($opendirectory = opendir("$directory/initial_participants")) {
-    while (($file = readdir($opendirectory)) !== false) {
-      if(!unlink("$directory/initial_participants/$file")) {
-          echo "Couldn’t delete $directory/initial_participants/$file.";
-      }
-    }
-    closedir($opendirectory);
-} else {
-    echo "Couldn't open $directory/initial_participants.";
-}
-rmdir("$directory/initial_participants");
-
-// Show contents after deletion of initial participants.
-if ($opendirectory = opendir($directory)) {
-    while (($file = readdir($opendirectory)) !== false) {
-      echo "directory: $file<br />";
-    }
-    closedir($opendirectory);
-} else {
-  echo "<em><strong>$directory is missing!</strong></em>";
-}
-
+  if (!isset($_POST['pass'])) {
 ?>
+                  <form action="lookat_uploads.php" method=POST>
+                    Enter password to view current results.<br />
+                    <label for="pass">Password:</label><br />
+                    <input id="pass" name="pass" type="password" /><br /><br />
+                    <input type="submit" value="View" />
+                  </form>
+<?php
+          // I used password hash to encrypt password.
+  } elseif (password_verify($_POST['pass'], '$2y$10$EuvvcNeKvie1HZcgqDMP6OyQz0yKkBOvViwEZ7VMudZloQFdp.0pu')) {
+    $arr = array('initial_participants', 'feedback', 'answers');
+    foreach ($arr as $this_dir) {
+      echo "<h3>$this_dir:</h3>";
+      $cur_dir = "$directory/$this_dir";
+      if (is_dir($cur_dir)){
+        if ($opendirectory = opendir($cur_dir)) {
+          while (($file = readdir($opendirectory)) !== false) {
+            echo "filename: $file<br />";
+          }
+          closedir($opendirectory);
+        } else {
+            echo "<em><strong>$cur_dir is missing!</strong></em>";
+        }
+      }
+    }
+
+    // unlink("$uploads_dir/$filename");
+
+  } else { // wrong password
+?>
+                <h3>Password incorrect.</h3>
+                <form action="download_results.php" method=POST>
+                  Enter password to download current results.<br />
+                  <label for="pass">Password:</label><br />
+                  <input id="pass" name="pass" type="password" /><br /><br />
+                  <input type="submit" value="Download" />
+                </form>
+<?php
+  }
+?>
+                </div>
+              </div>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
+
+
