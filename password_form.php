436c952d9146391342ecf04257f20665fa6a33e8
Eric Ford <ericford@mac.com>
2021-04-21 15:11:52 -0400

    Added password_form.php.

---
+++ password_form.php
@@ -0,0 +1,6 @@
+<h3><?php echo $form_head; ?></h3>
+<form action="<?php echo $form_action; ?>" method=POST>
+  <?php echo $form_text; ?><br />
+  <input id="pass" name="pass" type="password" /><br /><br />
+  <input type="submit" value="<?php echo $submit_text; ?>" />
+</form>
