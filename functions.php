1a51d925bbe7ad3c56d4f36f44c3d7239dd0fed7
Eric Ford <ericford@mac.com>
2021-09-06 10:08:40 -0400

    Trying to figure out what's not getting deleted in participants directory.

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
       if ($dir = opendir($dir_name)) {
         while (($file_name = readdir($dir)) !== false) {
           $file_path = "$dir_name/$file_name";
-          if (substr($file_name, 0, 1) != '.') {
+          if (!is_dir("$cur_results_dir/$file")) {
             if (is_dir($file_path)) {
               clear_directory($file_path);
             } else {
