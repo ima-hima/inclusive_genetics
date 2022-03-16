05a000f810cfceb704a2c80114d83271b7d011e3
ima-hima <ericford@mac.com>
2022-03-16 01:07:11 -0400

    Updated view_results.php to show zipped files at top level, as in test.

--- download_zipped_dir.php
+++ download_zipped_dir.php
@@ -1,18 +1,9 @@
 <?php
-  require('functions.php');
-  if (!isset($_GET['pass'])) {
-    require('header.php');
-    $form_action = 'download_results.php';
-    $submit_text = 'Download';
-    $form_head = '';
-    $form_text = 'Enter password to download current results.';
-    require('password_form.php');
-          // I used password hash to encrypt password.
-  } elseif (password_verify($_GET['pass'], $password_hash)) {
+    // require('header.php');
     $dt = new DateTime('NOW');
     $now = $dt->format('Y-m-d');
     $archive_filename = "project-inclusive_results_$now.tar.gz";
-    exec("tar -zcvf $results_directory/$archive_filename $results_directory/zipped");
+    // exec("tar -zcvf $results_directory/$archive_filename $results_directory/zipped");
 
     header_remove();
     header('Content-Description: File Transfer');
@@ -26,11 +17,5 @@
     flush();
     readfile("$results_directory/$archive_filename");
 
-  } else { // wrong password
-    $form_head = 'Password incorrect';
-    $form_text = 'Enter password to download current results';
-    $submit_text = 'Download';
-    require('password_form.php');
-  }
   require('footer.php');
 ?>
--- view_results.php
+++ view_results.php
@@ -14,7 +14,36 @@
     $sub_dirs = array('initial_participants' => 'Initial Participants',
                       'feedback' => 'IAT Feedback',
                       'answers' => 'Final output');
-    foreach ($sub_dirs as $sub_dir => $description) {
+    // First view any files that are in top level of uploads directory.
+    // Because there are always folders present we need to look specifically
+    // for files. Excluding .htaccess.
+    $opendirectory = opendir($results_directory);
+    $not_empty = false;
+    while (($file = readdir($opendirectory)) !== false) {
+      if (!is_dir("$results_directory/$file")) {
+        $not_empty = true;
+        break;
+      }
+    }
+    closedir($opendirectory);
+    if ($not_empty) {
+      $opendirectory = opendir($results_directory);
+      echo "<h3>Uploads:</h3>";
+      echo "<table><tr><th>File name</th><th>Permissions</th><th>Size</th><th>Creation date</th>";
+      while (($file = readdir($opendirectory)) !== false) {
+        if (!is_dir("$results_directory/$file")) {
+          $creation_time = date("F d, Y H:i", filectime("$results_directory/$file"));
+          $filesize = filesize("$results_directory/$file");
+          $perms = substr(decoct(fileperms("$results_directory/$file")),3);
+          echo "<tr><td>$file</td><td>$perms</td><td>$filesize</td><td><span class=\"time\">$creation_time</span></td></tr>";
+        }
+      }
+      echo "</table>";
+      closedir($opendirectory);
+    } else {
+      echo '<p>There are no .zip files to download.</p>';
+    }
+    // Now display file info for all results from studies.    foreach ($sub_dirs as $sub_dir => $description) {
       echo "<h3>$description:</h3>";
       $cur_dir = "$results_directory/$sub_dir";
       if (is_dir($cur_dir)){
