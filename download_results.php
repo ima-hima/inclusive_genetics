e41f7a2d6cdbd76fdfdf46d27cb4c73a34230f58
ima-hima <ericford@mac.com>
2022-04-11 23:20:29 -0400

    Changed conditional flow of download_results.php. More important, removed footer call after download has started.

--- download_results.php
+++ download_results.php
@@ -19,28 +19,28 @@
     $form_text = 'Enter password to download current results';
     $submit_text = 'Download';
     require('password_form.php');
-  } else {
-    $dt = new DateTime('NOW');
-    $now = $dt->format('Y-m-d');
-    $archive_filename = "project-inclusive_results_$now.tar.gz";
-    $archive_file = "$results_directory/$archive_filename";
-    exec("mkdir $results_directory/zipped/");
-    foreach ($sub_dirs as $sub_dir => $description) {
-      exec("cp $results_directory/$sub_dir $results_directory/zipped");
-    }
-    exec("tar -zcvf $archive_file $results_directory/zipped");
-
-    header_remove();
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/gzip');
-    header('Content-Disposition: attachment; filename="' . "$archive_filename" . '"');
-    header('Content-Transfer-Encoding: binary');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($archive_file));
-    flush();
-    readfile($archive_file);
+    require('footer.php');
   }
-  require('footer.php');
+  $dt = new DateTime('NOW');
+  $now = $dt->format('Y-m-d');
+  $archive_filename = "project-inclusive_results_$now.tar.gz";
+  $archive_file = "$results_directory/$archive_filename";
+  exec("mkdir $results_directory/zipped/");
+  foreach ($sub_dirs as $sub_dir => $description) {
+    exec("cp $results_directory/$sub_dir $results_directory/zipped");
+  }
+  exec("tar -zcvf $archive_file $results_directory/zipped");
+
+  header_remove();
+  header('Content-Description: File Transfer');
+  header('Content-Type: application/gzip');
+  header('Content-Disposition: attachment; filename="' . "$archive_filename" . '"');
+  header('Content-Transfer-Encoding: binary');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+  header('Pragma: public');
+  header('Content-Length: ' . filesize($archive_file));
+  flush();
+  readfile($archive_file);
+  // require('footer.php');
 ?>
